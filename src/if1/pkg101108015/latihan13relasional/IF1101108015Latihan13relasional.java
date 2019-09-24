09ff5f931d46b675c75f95fc2de9fd8876dce34f
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:44:03 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan13relasional/IF1101108015Latihan13relasional.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan13relasional;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program relasional
+ */
+public class IF1101108015Latihan13relasional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1==bil2));
+        System.out.println("a != b = " + (bil1!=bil2));
+        System.out.println("a > b  = " + (bil1 > bil2));
+        System.out.println("a < b  = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
